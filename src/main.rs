66c2798002ffcfc3acde72447e50d24390fbfed6
Andrew Reeves <andrew@Andrews-MacBook-Pro.local>
2024-04-03 20:46:29 +0200

    working thread with frame number

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,51 @@
-// use ndarray::Array;
+use ndarray::{Array, Array2d};
 // use ndarray_rand::RandomExt;
 // use ndarray_rand::rand_distr::{Normal, Uniform};
 use core::time;
+use std::sync::{Arc, Mutex};
 use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
 // use std::time::Instant;
 use std::thread;
 use std::option;
 
 pub struct Camera {
-    frame_number: AtomicU64,
-    acquiring: AtomicBool,
+    n_rows: u32,
+    n_cols: u32,
+    frame_number: Arc<AtomicU64>,
+    acquiring: Arc<AtomicBool>,
     thread_handle: option::Option<thread::JoinHandle<()>>,
+    frame_buffer: Arc<Mutex<Vec<u16>>>,
 }
 
 impl Camera {
-    pub fn new() -> Self {
+    pub fn new(n_rows: u32, n_cols: u32) -> Self {
+        let frame_buffer = Arc::new(Mutex::new(
+            Array2d
+        ));
         Self{
-            frame_number: AtomicU64::new(0),
-            acquiring: AtomicBool::new(false),
+            n_rows: n_rows,
+            n_cols: n_cols,
+            frame_number: Arc::new(AtomicU64::new(0)),
+            acquiring: Arc::new(AtomicBool::new(false)),
             thread_handle: None
+
         }
     }
 
     pub fn start_acquisition(&mut self) {
         println!("Start Acquisition");
-        self.acquiring.store(true, Ordering::Relaxed);
+        
+        let fn_ref = Arc::clone(&self.frame_number);
+        let acq_ref = Arc::clone(&self.acquiring);
+
+        acq_ref.store(true, Ordering::Relaxed);
 
         self.thread_handle = option::Option::Some(std::thread::spawn(move ||{
-            while self.acquiring.load(Ordering::Relaxed) {
+            while acq_ref.load(Ordering::Relaxed) {
                 thread::sleep(time::Duration::from_secs(1));
-                let fr = self.frame_number.load(Ordering::Relaxed);
-                self.frame_number.store(fr+1, Ordering::Relaxed);
+
+                let fr = fn_ref.load(Ordering::Relaxed);
+                fn_ref.store(fr+1, Ordering::Relaxed);
             }
         }));
     }
@@ -42,14 +57,14 @@ impl Camera {
     }
 }
 
-
 fn main() {
-
-
     println!("Hello, Camera!");
-
     println!("Init Camera...");
-    let mut cam = Camera::new();
+
+    let rows = 512;
+    let cols = 640;
+
+    let mut cam = Camera::new(rows, cols);
     println!("Init Camera...Done");
     
     // println!("Cam Acquiring:    {}", cam.acquiring);
@@ -65,13 +80,6 @@ fn main() {
     cam.stop_acquisition();
     let fr = cam.frame_number.load(Ordering::Relaxed);
     println!("Frame Number: {}", fr);
-
-
-
-
-    // let rows = 5120;
-    // let cols = 6400;
-    // let n_iters = 100;
 
     // let t_start = Instant::now();
 
